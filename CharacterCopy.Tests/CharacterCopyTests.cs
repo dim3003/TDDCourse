2a276a4550b3ec4750fcaae41655e7ab4d156e71
dimitri <andre.d3003@gmail.com>
2026-03-25 17:23:44 +0100

    adding tests

--- CharacterCopy.Tests/CharacterCopyTests.cs
+++ CharacterCopy.Tests/CharacterCopyTests.cs
@@ -65,6 +65,87 @@ public class CharacterCopyTests
             destination.DidNotReceive().WriteChar('\n');
         }
 
+        [TestCase('a', 'x', '\n')]
+        [TestCase('u', 'v', 'e', 'm', 'd', '\n')]
+        [TestCase('!', ',', '2', 'd', '\n')]
+        public void GivenManyCharactersBeforeNewLine_ShouldWriteThoseCharactersInOrder(
+            char firstChar,
+            params char[] nextChars)
+        {
+            // Arrange
+            ISource source = CreateSource(firstChar, nextChars);
+
+            var destination = Substitute.For<IDestination>();
+
+            var sut = new Copier(source, destination);
+
+            // Act
+            sut.Copy();
+
+            // Assert
+            var allExpected = nextChars.Prepend(firstChar).Where(c => c != '\n');
+            Received.InOrder(() =>
+            {
+                foreach (var c in allExpected)
+                {
+                    destination.WriteChar(c);
+                }
+            });
+            destination.DidNotReceive().WriteChar('\n');
+        }
+
+        [TestCase('a', 'a', '\n')]
+        [TestCase('!', ',', '2', '2', 's', '\n')]
+        public void GivenRepeatedCharactersBeforeNewLine_ShouldWriteThoseCharacters(
+            char firstChar,
+            params char[] nextChars)
+        {
+            // Arrange
+            ISource source = CreateSource(firstChar, nextChars);
+
+            var destination = Substitute.For<IDestination>();
+
+            var sut = new Copier(source, destination);
+
+            // Act
+            sut.Copy();
+
+            // Assert
+            var allExpected = nextChars.Prepend(firstChar).TakeWhile(c => c != '\n');
+            foreach (var group in allExpected.GroupBy(c => c))
+            {
+                destination.Received(group.Count()).WriteChar(group.Key);
+            }
+            destination.DidNotReceive().WriteChar('\n');
+        }
+
+        [TestCase('a', '\n', 'b')]
+        [TestCase('!', ',', '2', 'f', 's', '\n', 'u', 'u')]
+        public void GivenCharactersAfterNewLine_ShouldWriteOnlyCharactersBeforeNewLine(
+            char firstChar,
+            params char[] nextChars)
+        {
+            // Arrange
+            ISource source = CreateSource(firstChar, nextChars);
+
+            var destination = Substitute.For<IDestination>();
+
+            var sut = new Copier(source, destination);
+
+            // Act
+            sut.Copy();
+
+            // Assert
+            var allExpected = nextChars.Prepend(firstChar).TakeWhile(c => c != '\n');
+            foreach (var group in allExpected.GroupBy(c => c))
+            {
+                destination.Received(group.Count()).WriteChar(group.Key);
+            }
+            destination.DidNotReceive().WriteChar('\n');
+        }
+
+
+
         [TestCase('\n')]
         public void GivenNoCharacterBeforeNewLine_ShouldWriteNothing(
             char firstChar,
